e9e99f12bf17707cc7ddfb98e2c280417871af6c
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2025-12-22 18:17:05 +0300

    create: create our baisc shell program

---
+++ main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+int main(int argc, char **argv)
+{
+	char *buffer = NULL, *storge[100];
+	size_t len = 0;
+	ssize_t nread;
+	int line_count = 0, pid;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			printf("($) ");
+		nread = getline(&buffer, &len, stdin);
+		if (nread == -1)
+			break;
+		if (buffer[nread - 1] == '\n')
+			buffer[nread - 1] = '\0';
+		if (buffer[0] == '\0')
+			continue;
+		line_count++;
+		storge[0] = buffer;
+		storge[1] = NULL;
+		if (access(buffer, X_OK) != -1)
+		{
+			pid = fork();
+			if (pid == -1)
+			{
+				perror("Error:");
+				return (-1);
+			}
+			if (pid >= 1)
+				wait(NULL);
+			if (pid == 0)
+			{
+				if (execve(buffer, storge, environ) == -1)
+				{
+					fprintf(stderr, "%s: %d: %s: not found\n", argv[0], line_count, buffer);
+					exit(EXIT_FAILURE);
+				}
+			}
+		}
+		else
+			fprintf(stderr, "%s: No such file or directory\n", argv[0]);
+	}
+	free(buffer);
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHEEL_H
+#define SHEEL_H
+
+extern char **environ;
+
+
+
+
+
+
+
+
+#endif /*SHEEL_H*/
