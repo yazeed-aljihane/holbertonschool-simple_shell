20b755d94ec9e38d9c63e4780e2ad2579f88940a
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2025-12-22 18:21:59 +0300

    create: create our baisc shell program

--- main.c
+++ main.c
@@ -1,8 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
+#include "shell.h"
+
 int main(int argc, char **argv)
 {
 	char *buffer = NULL, *storge[100];
--- shell.h
+++ shell.h
@@ -1,9 +1,11 @@
 #ifndef SHEEL_H
 #define SHEEL_H
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
 extern char **environ;
-
-
 
 
 
